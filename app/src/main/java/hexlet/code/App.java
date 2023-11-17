371824724a7ac209db48cb5923b55b6e4166948d
KermittheFroggg <santamantana@gmail.com>
2023-11-17 12:49:09 +0100

    step 8

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -53,6 +53,7 @@ public class App {
 
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+        config.setMaximumPoolSize(5);
         return new HikariDataSource(config);
     }
 
